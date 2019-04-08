531220e6e389c86ddd96e94c1202cf89987fd46f
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-08 14:09:34 +0800

    调试

---
+++ 随意调试程序堆/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+std::string global_str;
+int global_int;
+int main ()
+{
+	int local_int;
+	std::string local_str;
+	if(global_str[0]=='\0');
+//	std::cout<<global_str<<std::endl;
+	std::cout<<global_int<<std::endl;
+	std::cout<<local_int<<std::endl;
+	return 0;
+ } 
