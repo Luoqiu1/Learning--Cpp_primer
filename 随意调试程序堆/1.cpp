470bec87b688f9eae23c19c4a46f4ee70f040d06
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-09 13:12:35 +0800

    调试

--- 随意调试程序堆/1.cpp
+++ 随意调试程序堆/1.cpp
@@ -14,5 +14,25 @@ int main ()
 	std::cout<<*q<<std::endl;
 	b=4;
 	std::cout<<*q<<std::endl;
+	
+	int c=5;
+	const int d=6;
+	int *const m=&c;
+	
+	const int *const n=&c;
+	c=2;
+	std::cout<<*n<<std::endl;
+	c=7;
+	std::cout<<*n<<std::endl;
+	
+	const int k=5,&s=k;
+//	s=4;
+	const int *w;
+	w=&c;
+	std::cout<<*w<<std::endl;
+//	*w=2;
+	w=&b;
+	std::cout<<*w<<std::endl;
+//	int *const w;
 	return 0;
  } 
