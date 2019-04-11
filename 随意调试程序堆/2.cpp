3cf5df3a95e78f11eaa1cf10d7f643b2966828c5
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-11 23:21:28 +0800

    调试

--- 随意调试程序堆/2.cpp
+++ 随意调试程序堆/2.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int k=2;
+constexpr int go(int i,int j)
+{
+	return i+j;
+}
 int main ()
 {
-	int *p=(int*)malloc(5*sizeof(int));
-	for(int i=0;i<5;i++){
-		printf("%p\n",p[i]);
-	}
+	int i=2;
+	const int c=2,d=5;
+	constexpr int a=go(c,d);
+	constexpr int *p=&k;
+	const int *q;
+	q=&i;
+//	*q=4;
+	i=4;
+	*p=6;
+	int null=0;
+	int *m=&null;
+//	p=&i;
+	cout<<*q; 
 	return 0;
 }
