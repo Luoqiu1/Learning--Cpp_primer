2de6398eead12bd772fb3b3b7a6312ca13178bbd
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-18 06:48:37 +0800

    探索[]

--- 第三章/3.2.3.cpp
+++ 第三章/3.2.3.cpp
@@ -4,15 +4,29 @@ using namespace std;
 
 int main ()
 {
-	string s1="nihou,aw!@>>>..wea";
+	string s1;
 //	for(auto ch:s1){
 //		cout<<ch<<' ';
 //	}
 //	int cnt=0;²»Ì«ºÃ
-	decltype(s1.size()) cnt=0; 
-	for(auto ch:s1){
-		if(ispunct(ch))cnt++;
+//	decltype(s1.size()) cnt=0; 
+//	for(auto ch:s1){
+//		if(ispunct(ch))cnt++;
+//	}
+//	cout<<cnt;
+	getline(cin,s1);
+	for(auto &ch:s1){
+		if(ch>='a'&&ch<='z')ch+='A'-'a';
+	} 
+	cout<<s1;
+	int a[5]={1,2,3};
+	for(auto x:a){
+		cout<<x<<' ';
 	}
-	cout<<cnt;
+	cout<<endl;
+	const char *p="nihao";
+	p=p+2;
+	int i=-1;
+	cout<<p[i];
 	return 0;
 }
