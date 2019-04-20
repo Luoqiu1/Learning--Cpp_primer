5569f0e57a9f2713bd004c665b2f4acbb40d5906
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-20 21:43:36 +0800

    探条件运算符

--- 第三章/4.2.cpp
+++ 第三章/4.2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 int main()
 {
@@ -16,13 +17,31 @@ int main()
 //	int i;
 //	if(42=i)cout<<"yes";ÐèÒª×óÖµ 
 //	if(i=123124)cout<<"yes";
-	int i;double d;
-	d=i=3.5; 
-	cout<<d<<' '<<i<<endl;
-	double b=3.0;
-//	cout<<b;
-//	printf("%f",b);
-	int c;
-	if(!(c=0))cout<<"yes";
+
+//	int i;double d;
+//	d=i=3.5; 
+//	cout<<d<<' '<<i<<endl;
+//	double b=3.0;
+////	cout<<b;
+////	printf("%f",b);
+//	int c;
+//	if(!(c=0))cout<<"yes";
+	
+//	vector<int> ivec;
+//	decltype(ivec.size()) x;
+//	while(cin>>x)ivec.push_back(x);
+//	for(auto &x:ivec)if(x%2==1)x*=2;
+//	for(auto x:ivec)cout<<x<<' ';
+	
+	int grade;while(cin>>grade){
+//	string s1=grade>75?grade>90?"high pass":"low pass":grade>=60?"pass":"fail";cout<<s1<<endl;
+	string s1;if(grade>=60){
+		if(grade>75){
+			if(grade>90)s1="high pass";
+			else s1="low pass";
+		}
+		else s1="pass";
+	}
+	else s1="fail";cout<<s1<<endl;}
 	return 0;
- } 
+ }
