25d0c1465da0d319066028defe539fd3a5168444
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-20 17:53:38 +0800

    4.4

--- 第三章/4.2.cpp
+++ 第三章/4.2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
@@ -6,7 +7,22 @@ int main()
 //	while(cin>>a>>b){
 //		cout<<a%b<<endl;
 //	}
-	
-	cout<<-30*3+21/5<<' '<<-30+3*21/5<<' '<<30/3*21%5<<' '<<-30/3*21%4;
+//	int val;
+//	while(cin>>val&&val!=42);
+//	double val{2.4};cout<<val<<endl;
+//	int val2{val};cout<<val2;//被警告了
+//	vector<int> vi{1,2,3};
+//	vi={0,1,2,3,4,5}; for(auto x:vi)cout<<x<<' ';
+//	int i;
+//	if(42=i)cout<<"yes";需要左值 
+//	if(i=123124)cout<<"yes";
+	int i;double d;
+	d=i=3.5; 
+	cout<<d<<' '<<i<<endl;
+	double b=3.0;
+//	cout<<b;
+//	printf("%f",b);
+	int c;
+	if(!(c=0))cout<<"yes";
 	return 0;
  } 
