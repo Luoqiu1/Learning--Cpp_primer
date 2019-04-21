c0bd7b3ccc2b92fd4f8697f003a45ec73259c27a
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-22 00:08:57 +0800

    调试

---
+++ 第四章/4.9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main ()
+
+{
+	string s1;vector<string> ivec;
+	auto a=sizeof(s1),b=sizeof(ivec);
+	cout<<a<<' '<<b;
+	return 0;
+ } 
