8beaab285f6e1dbf778bc713cf540b0dcb608537
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-28 16:54:44 +0800

    5.5

--- 第五章/1.4.cpp
+++ 第五章/1.4.cpp
@@ -14,5 +14,21 @@ int main ()
 //	scores.push_back("S");
 //	scores[2]="S"; 
 	//以上两种形式容器 加了const 长度和内容都不能改！即 容器 这一整体是常量！ 
+	
+	const vector<string> scores{"F","D","C","B","A","A++"};
+	int x;string MyScore;
+	while(cin>>x){
+		if(x>=0&&x<=100){
+			if(x<60)MyScore=scores[0];
+			else if(x==100)MyScore=scores[5];
+			else{
+				if(x%10>7) MyScore=scores[x/10-5]+'+';
+				else if(x%10<3) MyScore=scores[x/10-5]+'-';
+				else MyScore=scores[x/10-5];
+			}
+			cout<<MyScore<<endl;
+		}
+		else cout<<"Error Input!"<<endl;
+	}
 	return 0;
  } 
