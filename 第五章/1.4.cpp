9bf9f8327014e594fb4d3c557ff28e911cf42405
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-28 16:35:58 +0800

    const与vector
    以上两种形式容器 加了const 长度和内容都不能改！即 容器 这一整体是常量

--- 第五章/1.4.cpp
+++ 第五章/1.4.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main ()
 {
-	int sum=0,val=1;
-	while(val<=10)sum+=val,++val;
-	cout<<"Sum of 1 to 10 inclusive is "<<sum<<endl;
+//	int sum=0,val=1;
+//	while(val<=10)sum+=val,++val;
+//	cout<<"Sum of 1 to 10 inclusive is "<<sum<<endl;
+
+//	const vector<string> scores{"F","D","C","B","A","A++"};
+//	vector<string> const scores{"F","D","C","B","A","A++"};
+//	scores.push_back("S");
+//	scores[2]="S"; 
+	//以上两种形式容器 加了const 长度和内容都不能改！即 容器 这一整体是常量！ 
 	return 0;
  } 
