b84211d1e0f2765eaf199a9d8d51ce64e463792b
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-28 17:28:27 +0800

    5.9 5.10

--- 第五章/1.4.cpp
+++ 第五章/1.4.cpp
@@ -15,20 +15,39 @@ int main ()
 //	scores[2]="S"; 
 	//以上两种形式容器 加了const 长度和内容都不能改！即 容器 这一整体是常量！ 
 	
-	const vector<string> scores{"F","D","C","B","A","A++"};
-	int x;string MyScore;
-	while(cin>>x){
-		if(x>=0&&x<=100){
-			if(x<60)MyScore=scores[0];
-			else if(x==100)MyScore=scores[5];
-			else{
-				if(x%10>7) MyScore=scores[x/10-5]+'+';
-				else if(x%10<3) MyScore=scores[x/10-5]+'-';
-				else MyScore=scores[x/10-5];
-			}
-			cout<<MyScore<<endl;
+//	const vector<string> scores{"F","D","C","B","A","A++"};
+//	int x;string MyScore;
+//	while(cin>>x){
+//		if(x>=0&&x<=100){
+//			if(x<60)MyScore=scores[0];
+//			else if(x==100)MyScore=scores[5];
+//			else{
+//				if(x%10>7) MyScore=scores[x/10-5]+'+';
+//				else if(x%10<3) MyScore=scores[x/10-5]+'-';
+//				else MyScore=scores[x/10-5];
+//			}
+//			cout<<MyScore<<endl;
+//		}
+//		else cout<<"Error Input!"<<endl;
+//	}
+	
+	char ch;unsigned otherCnt=0;unsigned aCnt=0;unsigned eCnt=0;unsigned iCnt=0;unsigned oCnt=0;unsigned uCnt=0;
+//	while(cin>>ch){
+//		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')vowelCnt++;
+//	}
+//	cout<<vowelCnt<<endl;
+	while(cin>>ch){
+		if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')ch+='a'-'A';
+		switch(ch){
+			case'a':++aCnt;break;
+			case'e':++eCnt;break;
+			case'i':++iCnt;break;
+			case'o':++oCnt;break;
+			case'u':++uCnt;break;
+			default:++otherCnt;
 		}
-		else cout<<"Error Input!"<<endl;
 	}
+	cout<<"a："<<aCnt<<' '<<"e："<<eCnt<<' '<<"i："<<iCnt<<' '<<"o："<<oCnt<<' '<<"u："<<uCnt<<' '<<"other："<<otherCnt<<' ';
+
 	return 0;
  } 
