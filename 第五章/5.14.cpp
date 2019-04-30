a8a285aa75a1034cf452cdb6ec5fb9081b39b088
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-30 19:57:49 +0800

    5.17 前后缀问题

--- 第五章/5.14.cpp
+++ 第五章/5.14.cpp
@@ -5,22 +5,67 @@ using namespace std;
 
 int main ()
 {
-	string s0,pre,maxs;
-	unsigned cur=0,maxCnt=0;
-	while(cin>>s0){
-		if(s0==pre)cur++;
-		else {
-			if(cur>maxCnt){
-				maxCnt=cur;maxs=pre;
+	
+	
+//	string s0,pre,maxs;
+//	unsigned cur=0,maxCnt=0;
+//	while(cin>>s0){
+//		if(s0==pre)cur++;
+//		else {
+//			if(cur>maxCnt){
+//				maxCnt=cur;maxs=pre;
+//			}
+//			cur=0;
+//		}
+//		pre=s0;
+//	}
+//	if(cur>maxCnt){
+//				maxCnt=cur;maxs=pre;
+//	}
+//	if(maxCnt==0)cout<<"No"<<endl;
+//	else cout<<"The max is "<<maxs<<" lianxu occurs "<<maxCnt+1<<" times"<<endl;
+
+	vector<int>ivec1,ivec2;
+	int i;
+	while(cin>>i&&i!=999){
+		ivec1.push_back(i);
+	} 
+	while(cin>>i){
+		ivec2.push_back(i);
+	}
+	bool t1=true; 
+	bool flag=true;
+	bool eq=false;
+	if(ivec1.size()<ivec2.size()){
+		for(decltype(ivec1.size())i=0,sz=ivec1.size();i!=sz;i++){
+			if(ivec1[i]!=ivec2[i]){
+				flag=!flag;break;
 			}
-			cur=0;
 		}
-		pre=s0;
 	}
-	if(cur>maxCnt){
-				maxCnt=cur;maxs=pre;
+	else if(ivec2.size()<ivec1.size()){
+		t1=!t1;
+		for(decltype(ivec1.size())i=0,sz=ivec2.size();i!=sz;i++){
+			if(ivec1[i]!=ivec2[i]){
+				flag=!flag;break;
+			}
+		}
 	}
-	if(maxCnt==0)cout<<"No"<<endl;
-	else cout<<"The max is "<<maxs<<" lianxu occurs "<<maxCnt+1<<" times"<<endl;
+	else{
+		eq=true;
+		for(decltype(ivec1.size())i=0,sz=ivec2.size();i!=sz;i++){
+			if(ivec1[i]!=ivec2[i]){
+				flag=!flag;break;
+			}
+		}
+	}
+	if(flag){
+		if(eq)cout<<"两者相等";
+		else if(t1)cout<<"ivec1 is 前缀";
+		else cout<<"ivec2 is 前缀";
+	}
+	else{
+		cout<<"两者非前缀关系";
+	}
 	return 0;
 }
