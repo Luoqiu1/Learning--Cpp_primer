294e36aaca3647fe2267084e7018a429f72189c4
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-30 20:34:36 +0800

    重新及加深理解cin！
    如果你把一个basic_ios类的对象（cin就是）放到if语句的括号里，它就会被转换成void*型。如果输入失败的话，就会得到一个空指针（也就是0），那么if语句就不能通过。
    
    cin.clear() 可以重新置cin为有效！

--- 第五章/5.14.cpp
+++ 第五章/5.14.cpp
@@ -25,47 +25,66 @@ int main ()
 //	if(maxCnt==0)cout<<"No"<<endl;
 //	else cout<<"The max is "<<maxs<<" lianxu occurs "<<maxCnt+1<<" times"<<endl;
 
-	vector<int>ivec1,ivec2;
-	int i;
-	while(cin>>i&&i!=999){
-		ivec1.push_back(i);
-	} 
-	while(cin>>i){
-		ivec2.push_back(i);
-	}
-	bool t1=true; 
-	bool flag=true;
-	bool eq=false;
-	if(ivec1.size()<ivec2.size()){
-		for(decltype(ivec1.size())i=0,sz=ivec1.size();i!=sz;i++){
-			if(ivec1[i]!=ivec2[i]){
-				flag=!flag;break;
-			}
+
+//	vector<int>ivec1,ivec2;
+//	int i;
+//	while(cin>>i&&i!=999){
+//		ivec1.push_back(i);
+//	} 
+//	while(cin>>i){
+//		ivec2.push_back(i);
+//	}
+//	bool t1=true; 
+//	bool flag=true;
+//	bool eq=false;
+//	if(ivec1.size()<ivec2.size()){
+//		for(decltype(ivec1.size())i=0,sz=ivec1.size();i!=sz;i++){
+//			if(ivec1[i]!=ivec2[i]){
+//				flag=!flag;break;
+//			}
+//		}
+//	}
+//	else if(ivec2.size()<ivec1.size()){
+//		t1=!t1;
+//		for(decltype(ivec1.size())i=0,sz=ivec2.size();i!=sz;i++){
+//			if(ivec1[i]!=ivec2[i]){
+//				flag=!flag;break;
+//			}
+//		}
+//	}
+//	else{
+//		eq=true;
+//		for(decltype(ivec1.size())i=0,sz=ivec2.size();i!=sz;i++){
+//			if(ivec1[i]!=ivec2[i]){
+//				flag=!flag;break;
+//			}
+//		}
+//	}
+//	if(flag){
+//		if(eq)cout<<"两者相等";
+//		else if(t1)cout<<"ivec1 is 前缀";
+//		else cout<<"ivec2 is 前缀";
+//	}
+//	else{
+//		cout<<"两者非前缀关系";
+//	}
+		
+//	for(auto x:{"nihao","xixi","chongya"})cout<<x<<' '; 
+
+	string s1,s2;
+	do{
+		printf("请输入两个string串：");
+		cin>>s1>>s2;
+		if(s2=="stop"||s1=="stop"){
+			cout<<"the end";break;
 		}
-	}
-	else if(ivec2.size()<ivec1.size()){
-		t1=!t1;
-		for(decltype(ivec1.size())i=0,sz=ivec2.size();i!=sz;i++){
-			if(ivec1[i]!=ivec2[i]){
-				flag=!flag;break;
-			}
+		if(cin){
+			cout<<"输出较大的那个串：";
+			if(s1.size()>s2.size())cout<<s1;else cout<<s2;cout<<endl;
 		}
-	}
-	else{
-		eq=true;
-		for(decltype(ivec1.size())i=0,sz=ivec2.size();i!=sz;i++){
-			if(ivec1[i]!=ivec2[i]){
-				flag=!flag;break;
-			}
-		}
-	}
-	if(flag){
-		if(eq)cout<<"两者相等";
-		else if(t1)cout<<"ivec1 is 前缀";
-		else cout<<"ivec2 is 前缀";
-	}
-	else{
-		cout<<"两者非前缀关系";
-	}
+		cin.clear();
+//	}while(s2!="stop");
+	}while(cin); 
+	
 	return 0;
 }
