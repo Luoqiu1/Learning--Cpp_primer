33663a3d8f1504ae3ba23e26ccc6b6d62007a022
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-05-02 23:39:26 +0800

    调试

--- 第二章/2.6.3.cpp
+++ 第二章/2.6.3.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include "Sales_data.h"
 #include <string>//标准库函数肯定想到了我想的东西，必然存在头文件保护符！ 
+using namespace std;
+#include <stdexcept>
 int main()
 {
-	Sales_data data1,data2,data;
-	std::cin>>data1.bookNo>>data1.units_sold>>data1.revenue;
-	std::cin>>data2.bookNo>>data2.units_sold>>data2.revenue;
-	if(data1.bookNo==data2.bookNo){
-		data.bookNo=data1.bookNo;
-		data.units_sold=data1.units_sold+data2.units_sold;
-		data.revenue=data1.units_sold*data1.revenue+data2.units_sold*data2.revenue;
-		std::cout<<data.bookNo<<' '<<data.units_sold<<' '<<data.revenue;
-	}
-	else std::cout<<"Error!";
+//	Sales_data data1,data2,data;
+//	std::cin>>data1.bookNo>>data1.units_sold>>data1.revenue;
+//	std::cin>>data2.bookNo>>data2.units_sold>>data2.revenue;
+//	if(data1.bookNo==data2.bookNo){
+//		data.bookNo=data1.bookNo;
+//		data.units_sold=data1.units_sold+data2.units_sold;
+//		data.revenue=data1.units_sold*data1.revenue+data2.units_sold*data2.revenue;
+//		std::cout<<data.bookNo<<' '<<data.units_sold<<' '<<data.revenue;
+//	}
+//	else std::cout<<"Error!";
+	
+	Sales_data item1,item2;
+	while(cin>>item1>>item2){
+		try{
+			
+		}catch(runtime_error err){
+			cout<<err.what()<<"\nTry Again?Enter Y or N"<<endl;
+			char c;
+			cin>>c;
+			if(!cin||c=='n')break;
+		}
+	} 
 }
