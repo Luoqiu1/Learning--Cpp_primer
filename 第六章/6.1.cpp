5eea18a899f83914e202e203422dd473520336c8
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-05-03 22:26:19 +0800

    初探static

--- 第六章/6.1.cpp
+++ 第六章/6.1.cpp
@@ -12,10 +12,18 @@ int juedui(int x)//return x;
 	if(x<0)return -x;
 	return x;
 }
+int zeng()
+{
+	static int x=-1;
+	return ++x;
+}
 int main ()
 {
-	int x;cin>>x;
+	
+//	int x;cin>>x;
 //	cout<<fact(x)<<endl;
-	cout<<juedui(x)<<endl;
+//	cout<<juedui(x)<<endl;
+
+	for(int i=1;i<=10;i++)cout<<zeng()<<' ';
 	return 0;
 }
