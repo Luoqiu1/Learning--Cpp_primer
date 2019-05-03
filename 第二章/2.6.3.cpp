7d863eb1f1c8902c368a609f99cae34755579f95
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-05-03 11:25:18 +0800

    异常处理再尝试

--- 第二章/2.6.3.cpp
+++ 第二章/2.6.3.cpp
@@ -16,29 +16,43 @@ int main()
 //	}
 //	else std::cout<<"Error!";
 	
-	Sales_data item1,item2;
-	while(cin>>item1.bookNo>>item2.bookNo){
-		
-		try{
-			if(item1.bookNo!=item2.bookNo)
-				throw runtime_error("Data must refer to same ISBN");
-			cout<<"Equal"<<endl;
-		}catch(runtime_error err){
-	//	}catch(range_error err){
-			cout<<err.what() //err.what() 即初始化runtime_error对象时的初始值 
-							//此处为  "Data must refer to same ISBN"
-			<<"\nTry Again?Enter y or n"<<endl;
-			char c;
-			cin>>c;
-			if(!cin||c=='n')break;
-		//	if(c=='n')break; //该条语句用ctrl z也能退出 
-		}
-
-//		if(item1.bookNo!=item2.bookNo)
+//	Sales_data item1,item2;
+//	while(cin>>item1.bookNo>>item2.bookNo){
+//		
+//		try{
+//			if(item1.bookNo!=item2.bookNo)
 //				throw runtime_error("Data must refer to same ISBN");
 //			cout<<"Equal"<<endl;
-//		
-
+//		}catch(runtime_error err){
+//	//	}catch(range_error err){
+//			cout<<err.what() //err.what() 即初始化runtime_error对象时的初始值 
+//							//此处为  "Data must refer to same ISBN"
+//			<<"\nTry Again?Enter y or n"<<endl;
+//			char c;
+//			cin>>c;
+//			if(!cin||c=='n')break;
+//		//	if(c=='n')break; //该条语句用ctrl z也能退出 
+//		}
+//
+////		if(item1.bookNo!=item2.bookNo)
+////				throw runtime_error("Data must refer to same ISBN");
+////			cout<<"Equal"<<endl;
+////		
+//
+//	}
+	
+	
+	int a,b;
+	while(cin>>a>>b){
+		try{
+			if(b==0)throw runtime_error("error!");
+			cout<<a/b<<endl;
+		}catch(runtime_error err){
+		//	cout<<err.what()<<endl;
+			cout<<err.what()<<"\nTry Again? Enter y or n"<<endl;
+			char c;cin>>c;
+			if(!cin||c=='n')break;
+		}
 	}
 	return 0;
 }
