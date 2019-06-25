ae1a64ca81de36441980c63bc4621dbda89edd22
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-06-25 21:34:25 +0800

    初探initializer_list !
    initializer_list中的迭代器没有cbegin()和cend()，因为该对象中的元素永远（已经）是常量值！与vector不同！

--- 第六章/6.2.5.cpp
+++ 第六章/6.2.5.cpp
@@ -3,7 +3,7 @@
 int main (int argc,char **argv)
 {
 	std::string s;
-	for(decltype(s.size()) i=1;i!=argc;++i){
+	for(decltype(s.size()) i=1;i!=argc;++i){//注意是从argc[1]开始，argc[0]是用来保存程序的名字 
 		s+=argv[i]+' ';
 	}
 	std::cout<<s<<std::endl;
---
+++ 第六章/6.2.6.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <initializer_list>
+void print(std::initializer_list<std::string> lt)
+{
+	for(auto beg=lt.begin();beg!=lt.end();++beg){
+		std::cout<<*beg<<std::endl;
+	}
+}
+int main (int argc,char **argv)
+{
+	std::string s1,s2;
+	std::cin>>s1>>s2;
+	if(s1!=s2){
+		print({"Yes,GO!",s1,s2});
+	}
+	else print({"yes,GO!",s1});
+	return 0;
+}
