8ae874e48970aded3c97e717d85535b9a855668d
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-06-28 16:59:16 +0800

    函数指针实验！逐渐理解名字与优先级之间的关系。通过名字以及声明符结合优先级去理解！

--- 第六章/6.7.cpp
+++ 第六章/6.7.cpp
@@ -1,10 +1,42 @@
+#include <vector>
+#include <iostream>
+using namespace std;
 void func(const int i)
 {
 	return;
+}
+int Go(int,int);
+int Go1(int a,int b)
+{
+	return a+b;
+}
+int Go2(int a,int b)
+{
+	return a-b;
+}
+int Go3(int a,int b)
+{
+	return a*b;
+}
+int Go4(int a,int b)
+{
+	return a/b;
 }
 
 int main ()
 {
 	void (*f)(int)=func;
+	
+	vector<int (*)(int,int)> x;
+	vector<decltype(Go)*> xx;
+	x.push_back(Go1);x.push_back(Go2);x.push_back(Go3);x.push_back(Go4);
+	for(auto i:x){
+		cout<<i(10,5)<<' ';
+	}
+	cout<<endl;
+	xx.push_back(Go1);xx.push_back(Go2);xx.push_back(Go3);xx.push_back(Go4);
+	for(auto i:xx){
+		cout<<i(10,5)<<' ';
+	}
 	return 0;
  } 
