d921d297cefbc311d528d0fa9224f118713f17e2
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-07-08 17:23:49 +0800

    用ofstream向文件写东西时，会直接覆盖原来的内容（即先清空，再写）！若原来没有这个文件名，则会创建以该名为文件的文件！

--- 第八章/8.2.1.cpp
+++ 第八章/8.2.1.cpp
@@ -7,25 +7,61 @@ using namespace std;
 istream &read(istream &is,vector<string> &v)
 {
 	string s;
-	while(is>>s){
+	while(getline(is,s)){
 		v.push_back(s);
 	}
 	return is;
 }
 
-int main (int argc,char *argv[])
+void fun(vector<string> &v)
 {
-	string ifile="ifile.txt";
-	ifstream in(ifile);
-	vector<string> v;
-	if(in){
-		read(in,v);
-	}
-	in.close();
 	for(auto i:v)cout<<i<<endl;
 	cout<<endl;
 	for(auto beg=end(v)-1,ed=begin(v)-1;beg!=ed;--beg){
 		cout<<*beg<<endl;
 	}
+}
+
+ostream &print(ostream &os,istream &is)
+{
+	string s;
+	while(getline(is,s)){
+		os<<s<<endl;
+	}
+	return os;
+}
+
+int main (int argc,char *argv[])
+{
+	string ifile="ifile",ed=".txt";
+	ifstream in(ifile+ed);
+	vector<string> v,v2,v3;
+	if(in){
+		read(in,v);
+	}
+	in.close();
+	fun(v);
+	cout<<endl;
+	in.open(ifile+"2"+ed);
+	if(in){
+		read(in,v2);
+	}
+	in.close();
+	fun(v2);
+	cout<<endl<<endl<<endl<<endl<<endl;
+	
+	in.open(ifile+ed);
+	ofstream out(ifile+"5"+ed);
+	if(in&&out){
+		print(out,in);
+	}
+	out.close();
+	in.close();
+//	in.open(ifile+"2"+ed);
+//	if(in){
+//		read(in,v3);
+//	}
+//	in.close();
+//	fun(v3);
 	return 0;
 }
