5f14457a1c55ea37a2e854d032d81f31c4c43c08
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-07-09 15:53:51 +0800

    使用auto及对不同const(const对象和const迭代器)的理解

--- 第九章/9.2.1.cpp
+++ 第九章/9.2.1.cpp
@@ -21,22 +21,37 @@ vector<int>::iterator findIntP(vector<int>::iterator beg,vector<int>::iterator e
 
 int main ()
 {
-	vector<int> v1;
-	int a;
-	while(cin>>a){
-		v1.push_back(a);
-	}
-	cin.clear();
-	int f;
-	cin>>f;
-	bool flag=findInt(v1.cbegin(),v1.cend(),f);
-	if(flag)cout<<"Yes!";
-	else cout<<"None!";
+//	vector<int> v1;
+//	int a;
+//	while(cin>>a){
+//		v1.push_back(a);
+//	}
+//	cin.clear();
+//	int f;
+//	cin>>f;
+//	bool flag=findInt(v1.cbegin(),v1.cend(),f);
+//	if(flag)cout<<"Yes!";
+//	else cout<<"None!";
+//	cout<<endl;
+////	vector<int>::iterator &p=findIntP(v1.begin(),v1.end(),f);
+//	vector<int>::iterator p=findIntP(v1.begin(),v1.end(),f);
+//	if(p!=v1.end())cout<<"This point's address is "<<&p<<'.';
+//	else cout<<"Sorry.Don't find its address.";
+//	cout<<endl;
+
+	vector<int> v1{1,2,3};
+	const vector<int> v2{4,5,6};
+	auto it1=v1.begin();auto it2=v2.begin();
+	auto it3=v1.cbegin(),it4=v2.cbegin();
+	for(;it1!=v1.end();++it1)cout<<*it1<<' ';
 	cout<<endl;
-//	vector<int>::iterator &p=findIntP(v1.begin(),v1.end(),f);
-	vector<int>::iterator p=findIntP(v1.begin(),v1.end(),f);
-	if(p!=v1.end())cout<<"This point's address is "<<&p<<'.';
-	else cout<<"Sorry.Don't find its address.";
+	for(;it2!=v2.end();++it2)cout<<*it2<<' ';
 	cout<<endl;
+	for(;it3!=v1.end();++it3)cout<<*it3<<' ';
+	cout<<endl;
+	for(;it4!=v2.end();++it4)cout<<*it4<<' ';
+	cout<<endl;
+	
+	
 	return 0;
 }
