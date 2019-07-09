0cb2a4585e27ad8a56b1aefd26e532e12520fca5
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-07-09 21:05:08 +0800

    使用swap时，除了array会真的发生值的交换外（且比较慢，取决于array的问题规模），其他的容器swap操作很快——元素本身并未交换，只交换了两个容器的内部数据结构。
    故swap操作后，指针、引用、迭代器所绑定的元素保持不变！但由于array的元素值真正交换了，对应的指针、引用、迭代器所对应的值也会交换！

--- 第九章/9.2.4.cpp
+++ 第九章/9.2.4.cpp
@@ -6,20 +6,31 @@ using namespace std;
 
 int main () 
 {
-//	vector<int> v1;
+	vector<int> v1;
 //	int a;
 //	while(cin>>a){
 //		v1.push_back(a);
 //	}
 //	list<double> v2(v1.rbegin(),v1.rend());
 //	for(auto i:v2)cout<<i<<' ';
-	array<int,10> a1{1,2,3,4,5};
+	array<int,6> a1{1,2,3,4,5};
 //	array<int,9> a2{0};		//std中的数组array，其大小也属于类型的一部分！ 
-	array<int,10> a2{0};
+	array<int,6> a2{6,7,8,9,10};
+	array<int,0>::iterator it1=a1.begin()+1;//似乎对迭代器来说，数组的大小没有影响？ 
 	for(auto i:a1)cout<<i<<' ';cout<<endl;
 	for(auto i:a2)cout<<i<<' ';cout<<endl;
+	cout<<*it1<<endl;
 	swap(a1,a2);
 	for(auto i:a1)cout<<i<<' ';cout<<endl;
 	for(auto i:a2)cout<<i<<' ';cout<<endl;
+	cout<<*it1<<endl;
+	vector<const char*> s1{"hello!","zai ma","gogoGO!"};
+	vector<string> s{10,"www"};
+	for(auto i:s1)cout<<i<<' ';cout<<endl;
+	for(auto i:s)cout<<i<<' ';cout<<endl;
+	s.assign(s1.begin()+1,s1.end());
+	for(auto i:s1)cout<<i<<' ';cout<<endl;
+	for(auto i:s)cout<<i<<' ';cout<<endl;
+	cout<<v1.max_size()<<endl;
 	return 0;
 }
