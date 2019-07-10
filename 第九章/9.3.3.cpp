ff4d52403e3653410fac39d0177952af2138b9e1
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-07-10 12:03:32 +0800

    加深对迭代器的理解！对于删除之后的某容器为实例出发：
    	vector<int> v1{1,2,3,4,5};
    	vector<int>::iterator it1=v1.begin()+1;
    	vector<int>::iterator it2=v1.begin()+2;
    	v1.erase(v1.begin()+2,v1.end()-1);
    	cout<<*it1<<endl;
    	cout<<*it2<<endl;
    
    //最开始序列是{1,2,3,4,5}
    //*it1为2 *it2为3
    //删除子序列后为{1,2,5}
    //*it1为2 *it2为5

--- 第九章/9.3.3.cpp
+++ 第九章/9.3.3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <forward_list>
+#include <vector>
 using namespace std;
 
 int main ()
@@ -10,5 +11,17 @@ int main ()
 //	lst1.pop_back(); forward_list中无pop_back函数 
 						//不难理解为什么！结合数据结构的知识。 
 	for(auto i:lst1)cout<<i;cout<<endl;
+	
+	vector<int> v1{1,2,3,4,5};
+	vector<int>::iterator it1=v1.begin()+1;
+	vector<int>::iterator it2=v1.begin()+2;
+	cout<<endl<<*it1<<endl;
+	cout<<endl<<*it2<<endl;
+	cout<<endl<<*(v1.end()-1)<<endl;
+	v1.erase(v1.begin()+2,v1.end()-1);
+	for(auto i:v1)cout<<i<<' ';
+	cout<<endl;
+	cout<<*it1<<endl;
+	cout<<*it2<<endl;
 	return 0;
 }
