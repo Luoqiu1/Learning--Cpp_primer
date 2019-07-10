3afaae09ccb258793927507f05bf31ec995d4724
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-07-10 16:06:46 +0800

    添加元素操作返回值（最后一个插入的元素）——forward_list再实验。基于该容器的操作都是对迭代器之后的元素操作！insert或者emplace返回的都是最后一个插入的元素！
    例如输入1 2 3 4 5
    依次插入链表，均是从头插入。则结果看上去是 5 4 3 2 1 ！这里要仔细！

--- 第九章/9.3.4.cpp
+++ 第九章/9.3.4.cpp
@@ -1,26 +1,59 @@
 #include <iostream>
 #include <forward_list>
 #include <list>
+#include <string>
 using namespace std;
+
+void fun(forward_list<string> &flst,const string &s1,const string &s2)
+{
+	auto prev=flst.before_begin();
+	auto curr=flst.begin();
+	while(curr!=flst.end()){
+		if(*curr==s1){
+			flst.insert_after(curr,s2);
+			return;
+		}
+		else{
+			++prev;++curr;
+		//	++curr;
+		}
+	}
+	flst.emplace_after(prev,s2);
+}
 
 int main (int argc,char *argv[])
 {
-	forward_list<int> flst1{1,2,3,4,5,6,7};
+//	forward_list<int> flst1{1,2,3,4,5,6,7};
 //	auto it=flst1.before_begin();
 //	while(it!=flst1.end()-1){
 //		if(*(it+1)%2)flst1.erase_after(it);
 //		else ++it;
 //	}
 
-	auto prev=flst1.before_begin(),curr=flst1.begin();
-	while(curr!=flst1.end()){
-		if(*curr%2){
-			curr=flst1.erase_after(prev);
-		}
-		else{
-			++prev;++curr;
-		}
+//	auto prev=flst1.before_begin(),curr=flst1.begin();
+//	while(curr!=flst1.end()){
+//		if(*curr%2){
+//			curr=flst1.erase_after(prev);
+//		}
+//		else{
+//			++prev;++curr;
+//		}
+//	}
+//	for(auto i:flst1)cout<<i<<' ';cout<<endl;
+
+	forward_list<string> flst1;
+	string s;
+	decltype(flst1.before_begin()) text;
+	while(cin>>s){
+		text=flst1.insert_after(flst1.before_begin(),s);
+		//emplace°æ±¾¸üºÃ 
 	}
-	for(auto i:flst1)cout<<i<<' ';cout<<endl;
+	cout<<endl<<*text<<endl;
+	for(const auto &i:flst1)cout<<i<<' ';cout<<endl;
+	cin.clear();
+	string s1,s2;
+	cin>>s1>>s2;
+	fun(flst1,s1,s2);
+	for(const auto &i:flst1)cout<<i<<' ';cout<<endl;
 	return 0;
  } 
